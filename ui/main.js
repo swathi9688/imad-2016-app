221f771e7a270da2d0fe54deb49e91737de36700
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 14:29:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,5 @@
 console.log('Loaded!');
 
-//changes to main
-//var element= document.getElementById("main-text");
-//element.innerHTML= 'New value';
-
-//make th eimage to move
-//var img= document.getElementById("madi");
-//var marginLeft=0;
-
-//function moveRight() {
-    //marginLeft= marginLeft+ 1;
-    //img.style.marginLeft = marginLeft + 'px';
-//}
-//img.onclick = function () {
-    //var interval = setInterval (moveRight, 10);
-   // img.style.marginLeft = '100px';
-//}
 
 //button click me action
 var button= document.getElementById("counter");
