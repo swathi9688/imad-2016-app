11d6a3f279617b7d8ee5bf077c4fadc3104cad37
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 14:16:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,8 @@
-console.log('Loaded!');
 
-//changes to main
-//var element= document.getElementById("main-text");
-//element.innerHTML= 'New value';
-
-//make th eimage to move
-//var img= document.getElementById("madi");
-//var marginLeft=0;
-
-//function moveRight() {
-    //marginLeft= marginLeft+ 1;
-    //img.style.marginLeft = marginLeft + 'px';
-//}
-//img.onclick = function () {
-    //var interval = setInterval (moveRight, 10);
-   // img.style.marginLeft = '100px';
-//}
 
 //button click me action
 var button= document.getElementById("counter");
-var counter=0;
+
 
 button.onclick = function () {
   //create a request object
@@ -40,7 +23,6 @@ button.onclick = function () {
   //Make a Request
   
   //render the variable in correct span
-  counter =counter+1;
-//  var span= document.getElementById("count");
-//  span.innerHTML= counter.toString();
+    request.open('GET',"https://swathi9688.imad.hasura-app.io/counter",true);
+    request.send(null);
 };
