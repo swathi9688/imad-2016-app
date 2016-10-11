49c502bf03fed1dfaae01721d5b4870c4b1944ea
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 13:10:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ function moveRight() {
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval (moveRight, 100);
+    var interval = setInterval (moveRight, 10);
    // img.style.marginLeft = '100px';
 }
