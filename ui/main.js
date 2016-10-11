7db32a16014b893b4cef03a95cca17b612d28ec4
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 13:09:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ element.innerHTML= 'New value';
 
 //make th eimage to move
 var img= document.getElementById("madi");
+var marginLeft=0;
+
+function moveRight() {
+    marginLeft= marginLeft+ 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval (moveRight, 100);
+   // img.style.marginLeft = '100px';
 }
