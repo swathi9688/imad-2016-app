4b6775a2da74b899fcec45e615b8f9962238d7a0
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 18:25:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,19 @@ button.onclick = function () {
     request.open('GET',"http://swathi9688.imad.hasura-app.io/counter",true);
     request.send(null);
 };
+
+
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick = function(){
+    //make a request to the server
+    
+    //capture a list of name and render a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for (var i=0;i<names.length;i){
+        list+= '<li>' + names[i] + '</li>';
+    }
+};
