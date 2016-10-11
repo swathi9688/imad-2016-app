a24e038098f5645130b281601e2b4ab151ac9193
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 07:58:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,15 +97,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req, res){
+    var articleName=req.param.artincleName;
     res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/article-two', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
