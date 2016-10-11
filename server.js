aa736a189404c0af6f6688deb378abd45da2a0c7
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 18:45:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,6 +118,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
 app.get('/submit-name/:name', function (req, res) {
     var name = req.params.name;
     names.push(name);
