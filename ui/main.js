8556e1e6978f691a9d4ce36aba833aacbfea7631
swathi9688 <swathishastry9688@gmail.com>
2016-10-11 22:31:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,16 +30,31 @@ button.onclick = function () {
 var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
+
 submit.onclick = function() {
     //make a request to the server
-    
+      var request=new XMLHttpRequest();
+  
+  //capture the response and store it a variable
+  request.onreadystatechange = function() {
+      if(request.readystate === XMLHttpRequest.DONE) {
+          // take an action
+          if(request.status === 200){
+               var names = request.responseText;
+                var list = ' ';
+                for (var i=0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul=document.getElementById("namelist");
+                 ul.innerHTML = list;
+          }
+      }
+      //no action taken
+  };
+    //make a request
+    request.open('GET',"http://swathi9688.imad.hasura-app.io/sumit-name = " + name,true);
+    request.send(null);
     //capture a list of name and render a list
-    var names =["name1" , "name2" , "name3" , "name4"];
-    var list = ' ';
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul=document.getElementById("namelist");
-    ul.innerHTML = list;
+  
     
 };
